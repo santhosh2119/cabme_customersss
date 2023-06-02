321f2825a646d24dd75a4f89558eca7eb3c84b94
santhosh2119 <59635756+santhosh2119@users.noreply.github.com>
2023-06-02 11:24:18 +0530

    kkk

---
+++ android/app/src/main/kotlin/com/cabme/android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cabme.android
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+}
---
+++ lib/controller/on_boarding_controller.dart
@@ -0,0 +1,21 @@
+import 'package:cabme/model/onboarding_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  var selectedPageIndex = 0.obs;
+
+  bool get isLastPage => selectedPageIndex.value == onBoardingList.length - 1;
+  var pageController = PageController();
+
+  List<OnboardingModel> onBoardingList = [
+    OnboardingModel(
+        'assets/images/intro_1.jpeg', 'intro_title_one'.tr, 'into_dec_one'.tr),
+    OnboardingModel(
+        'assets/images/intro_2.jpeg', 'intro_title_two'.tr, 'into_dec_two'.tr),
+    OnboardingModel('assets/images/intro_3.jpeg', 'intro_title_three'.tr,
+        'into_dec_three'.tr),
+        OnboardingModel('assets/images/intro_4.jpeg', 'Enjoy Riding',
+        "Thanks for choosing us"),
+  ];
+}
